ac3d9e3d7eeabe4caf2d69ca1b34b00bebe662ef
pacoyang <yangbaigao@gmail.com>
2023-06-02 12:26:01 +0800

    feat: interact with uniswap contract

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import { FlashLoanSimpleReceiverBase } from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
-import { IPoolAddressesProvider } from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
-import { IERC20 } from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+import "./interfaces/IUniswapV2Router02.sol";
 
 contract FlashLoan is FlashLoanSimpleReceiverBase {
     address payable owner;
@@ -53,6 +54,19 @@ contract FlashLoan is FlashLoanSimpleReceiverBase {
             "Only the contract owner can call this function"
         );
         _;
+    }
+
+    function getUniswapPrice(
+        address routerAddress,
+        address sell_token,
+        address buy_token,
+        uint256 amount
+    ) external view returns (uint256) {
+        address[] memory pairs = new address[](2);
+        pairs[0] = sell_token;
+        pairs[1] = buy_token;
+        uint256 price = IUniswapV2Router02(routerAddress).getAmountsOut(amount, pairs)[1];
+        return price;
     }
 
     receive() external payable {}

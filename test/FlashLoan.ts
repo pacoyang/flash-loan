36896ae595f0afb0cecf4ad2898fc66bd1c93ccf
pacoyang <yangbaigao@gmail.com>
2023-06-09 14:36:40 +0800

    feat: make arbitrage

--- test/FlashLoan.ts
+++ test/FlashLoan.ts
@@ -3,11 +3,24 @@ import { ethers } from "hardhat";
 
 describe("Flash Loan", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const weth_address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
     const FlashLoan = await ethers.getContractFactory("FlashLoan");
-    const flashLoan = await FlashLoan.deploy("0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e");
+    const flashLoan = await FlashLoan.deploy(
+      // AAVE Mainnet PoolAddressesProvider
+      "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e",
+      // uniswap
+      "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+      // sushiswap
+      "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
+      // weth
+      weth_address,
+      // dai
+      "0x6b175474e89094c44da98b954eedeac495271d0f",
+    );
     console.info("Flash loan contract deployed:", flashLoan.address);
-    const balance = await flashLoan.getBalance("0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c");
-    console.info("AAVE USDC balance:", balance);
-    expect(balance).to.equal(0);
+    const tx = await flashLoan.requestFlashLoan(weth_address, 1);
+    // wait until the transaction mined
+    const result = await tx.wait();
+    console.info(result);
   });
 });
